f22d11a9a482df2cb83c68b934cb85226989a99e
yulei <yulei@addcn.com>
2020-04-18 22:37:13 +0800

    feat: 添加 deploy.sh 运行脚本

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:yuxiaolei1989/yuxiaolei1989.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
