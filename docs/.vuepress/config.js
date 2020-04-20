5ea735d72c38fe5cd6547d18e352c6af78628d0c
yulei <yulei@addcn.com>
2020-04-20 16:57:31 +0800

    fix: 使用peaceiris/actions-gh-pages@v3修复构建流程

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,29 @@
 module.exports = {
-    title: '余小磊的博客',
-    description: '前端工程师，技术爱好者，记录自己的学习内容以及技术思考',
+  title: '余小磊的博客',
+  description: '前端工程师，技术爱好者，记录自己的学习内容以及技术思考',
 
-    themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'TypeScript', link: '/ts/' },
-            { text: 'CSS3', link: '/css3/' }
-        ],
-        sidebar: {
-            '/ts/': [
-                {
-                    title: '首页',
-                    collapsable: false,
-                    path: '/ts/',
-                    children: [
-                        { title: '基础类型', path: 'base' },
-                        { title: '变量声明', path: 'variable-declarations' }
-                    ]
-                }
-            ]
-        }
-    }
-}
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'TypeScript', link: '/ts/' },
+      { text: 'CSS3', link: '/css3/' },
+    ],
+    sidebar: {
+      '/ts/': [
+        {
+          title: '简介',
+          collapsable: false,
+          path: '/ts/',
+        },
+        {
+          title: '基础',
+          collapsable: false,
+          children: [
+            { title: '基础类型', path: 'basic-types' },
+            { title: '变量声明', path: 'variable-declarations' },
+          ],
+        },
+      ],
+    },
+  },
+};
--- docs/ts/index.md
+++ docs/ts/index.md
@@ -1,1 +1,5 @@
 ## 什么是 TypeScript ？
+
+[TypeScript](https://www.typescriptlang.org/) 是 `JavaScript` 的超集，主要提供类型系统和 ES6 支持，能够为编写大型 `JavaScript` 程序提供静态类型检查，它由 `Microsoft` 开发，代码[开源在 GitHub ](https://github.com/Microsoft/TypeScript)上。
+
+## 安装 TypeScript
