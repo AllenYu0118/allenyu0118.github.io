481d7adb1f400c9fe0c806b363058f03c96209b1
yulei <yulei@addcn.com>
2020-04-26 09:16:57 +0800

    feat: add home page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    title: '余小磊的博客',
+    title: 'Allen Yu',
     description: '前端工程师，技术爱好者，记录自己的学习内容以及技术思考',
 
     themeConfig: {
         nav: [
             { text: 'Home', link: '/' },
             { text: 'TypeScript', link: '/ts/' },
-            { text: 'CSS3', link: '/css3/' },
+            { text: 'CSS3', link: '/css3/' }
         ],
         sidebar: {
             '/ts/': [
                 {
                     title: '简介',
                     collapsable: false,
-                    path: '/ts/',
+                    path: '/ts/'
                 },
                 {
                     title: '基础',
@@ -21,10 +21,11 @@ module.exports = {
                     children: [
                         { title: '原始数据类型', path: 'primitive-data-types' },
                         { title: '任意值', path: 'any' },
-                        { title: '类型推论', path: 'type-inference' },
-                    ],
-                },
-            ],
+                        { title: '类型推论', path: 'type-inference' }
+                    ]
+                }
+            ]
         },
-    },
+        lastUpdated: 'Last Updated'
+    }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,16 @@
-## 介绍
-> 前端工程师，技术爱好者，记录自己的学习内容以及技术思考
+---
+home: true
+heroImage: /cat.jpg
+# heroText: 余小磊的博客
+tagline: 前端工程师，技术爱好者，记录自己的学习内容以及技术思考
+# actionText: 快速上手 →
+# actionLink: /zh/guide/
+features:
+    - title: 持续学习
+      details: 自我驱动、有计划、有目的学习。
+    - title: 持续思考
+      details: 问题的本质、解决方案。
+    - title: 持续输出
+      details: 落地学习过程、落地思考过程、落地解决方案。
+footer: MIT Licensed | Copyright © 2018-present Allen Yu
+---
